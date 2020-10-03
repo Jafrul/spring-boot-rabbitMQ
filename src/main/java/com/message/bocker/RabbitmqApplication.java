9ae00c8deb4ce9067156cd8044022bfc9c60e953
Ahasan JAVA <ahasan@atilimited.net>
2020-10-03 14:40:07 +0600

    rabbitmq configure in spring boot application

---
+++ src/main/java/com/message/bocker/RabbitmqApplication.java
@@ -0,0 +1,14 @@
+package com.message.bocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqApplication.class, args);
+		System.out.println("RabbitMQ application running.....");
+	}
+
+}
---
+++ src/main/java/com/message/bocker/controller/RabbitMQWebController.java
@@ -0,0 +1,28 @@
+package com.message.bocker.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.message.bocker.configure.RabbitMQSender;
+import com.message.bocker.model.Employee;
+
+@RestController
+@RequestMapping(value = "/javainuse-rabbitmq/")
+public class RabbitMQWebController {
+
+	@Autowired
+	private RabbitMQSender rabbitMQSender;
+
+	@GetMapping(value = "/producer")
+	public String producer(@RequestParam("empName") String empName, @RequestParam("empId") String empId) {
+		Employee emp = new Employee();
+		emp.setEmpId(empId);
+		emp.setEmpName(empName);
+		rabbitMQSender.send(emp);
+		return "Message sent to the RabbitMQ " + emp.getEmpName() + " Successfully";
+	}
+
+}

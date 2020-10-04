3cc9c8767359234654a379b9abf24bf6489483d2
Ahasan JAVA <ahasan@atilimited.net>
2020-10-04 16:10:41 +0600

    add rabbitmq listener

--- src/main/java/com/message/bocker/configure/RabbitMQConfig.java
+++ src/main/java/com/message/bocker/configure/RabbitMQConfig.java
@@ -5,15 +5,16 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-
 
 @Configuration
 public class RabbitMQConfig {
@@ -26,7 +27,14 @@ public class RabbitMQConfig {
 
 	@Value("${javainuse.rabbitmq.routingkey}")
 	private String routingkey;
+	
+	@Value("${spring.rabbitmq.username}")
+	String username;
+	
+	@Value("${spring.rabbitmq.password}")
+	private String password;
 
+	
 	@Bean
 	Queue queue() {
 		return new Queue(queueName, false);
@@ -47,11 +55,41 @@ public class RabbitMQConfig {
 		return new Jackson2JsonMessageConverter();
 	}
 
-	
 	@Bean
 	public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
 		final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
 		rabbitTemplate.setMessageConverter(jsonMessageConverter());
 		return rabbitTemplate;
+	}
+
+	// create MessageListenerContainer using default connection factory
+	@Bean
+	MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory) {
+		SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+		simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
+		simpleMessageListenerContainer.setQueues(queue());
+		simpleMessageListenerContainer.setMessageListener(new RabbitMQListner());
+		return simpleMessageListenerContainer;
+
+	}
+
+	// create custom connection factory
+	@Bean
+	ConnectionFactory connectionFactory() {
+		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory("localhost");
+		cachingConnectionFactory.setUsername(username);
+		cachingConnectionFactory.setUsername(password);
+		return cachingConnectionFactory;
+	}
+
+	// create MessageListenerContainer using custom connection factory
+	@Bean
+	MessageListenerContainer messageListenerContainer() {
+		SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+		simpleMessageListenerContainer.setConnectionFactory(connectionFactory());
+		simpleMessageListenerContainer.setQueues(queue());
+		simpleMessageListenerContainer.setMessageListener(new RabbitMQListner());
+		return simpleMessageListenerContainer;
+
 	}
 }
